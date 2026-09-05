f2f059809e09efa8e96784b9340741e6bf8ee126
Biniam Beza <biniambeza544@gmail.com>
2026-09-05 19:27:34 +0300

    filter options and subcatagory filters added in the collection page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+          <img src={assets.logo} className='mb-5 w-32' alt="" />
+          <p className='w-full md:w-2/3 text-gray-600'>
+            Elevate your everyday style with our curated collections. We bring you high-quality, timeless fashion designed for comfort, durability, and modern elegance.
+          </p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+251993835149</li>
+            <li>contact@benmart.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024@ Benmart.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
